bc706389381727fc857c8bb566df454fd348c1c8
su-mahesh <maheshkangude@gmail.com>
2021-01-25 23:48:45 +0530

    ADDED: ability create queue an enqueue nodes

---
+++ src/main/java/com/bridgelabz/MyQueue.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class MyQueue {
+
+    private final LinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new LinkedList();
+    }
+
+    public void enqueue(MyNode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+
+    public INode peek() {
+        return myLinkedList.getHead();
+    }
+}
---
+++ src/test/java/com/bridgelabz/MyQueueTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+
+    @Test
+    public void given3Numbers_WhenAddedToQueue_ShouldHaveFirstAddedNode() {
+
+        MyNode<Integer> node1 = new MyNode<>(56);
+        MyNode<Integer> node2 = new MyNode<>(30);
+        MyNode<Integer> node3 = new MyNode<>(70);
+
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.enqueue(node1);
+        myQueue.enqueue(node2);
+        myQueue.enqueue(node3);
+
+        INode peek = myQueue.peek();
+
+        Assert.assertEquals(node1, peek);
+    }
+}
